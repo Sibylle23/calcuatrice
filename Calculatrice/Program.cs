3684427259d4cefbf192b0c9900e1b6542a675e7
sibylle <sibyllevanderus@gmail.com>
2023-01-23 14:37:59 +0100

    Vérification choix

--- Calculatrice/Program.cs
+++ Calculatrice/Program.cs
@@ -28,26 +28,30 @@ public class Program
         AfficherMenu();
 
         int choix = int.Parse(Console.ReadLine());
-
-
-        switch (choix)
-        {
-            case 1:
-                Console.WriteLine($"Résultat de l'addition {nombre1} + {nombre2} = " + Addition(nombre1, nombre2));
-                break;
-            case 2:
-                Console.WriteLine($"Résultat de la soustraction {nombre1} - {nombre2} = " + Soustraction(nombre1, nombre2));
-                break;
-            case 3:
-                Console.WriteLine($"Résultat de la mutltiplication {nombre1} x {nombre2} = " + Multiplication(nombre1, nombre2));
-                break;
-            case 4:
-                Console.WriteLine($"Résultat de la division {nombre1} : {nombre2} = " + Division(nombre1, nombre2));
-                break;
-            default:
-                Console.WriteLine("Veuillez choisir une opération");
-                break;
+        if (choix > 1 || choix < 4){
+            switch (choix)
+            {
+                case 1:
+                    Console.WriteLine($"Résultat de l'addition {nombre1} + {nombre2} = " + Addition(nombre1, nombre2));
+                    break;
+                case 2:
+                    Console.WriteLine($"Résultat de la soustraction {nombre1} - {nombre2} = " + Soustraction(nombre1, nombre2));
+                    break;
+                case 3:
+                    Console.WriteLine($"Résultat de la mutltiplication {nombre1} x {nombre2} = " + Multiplication(nombre1, nombre2));
+                    break;
+                case 4:
+                    Console.WriteLine($"Résultat de la division {nombre1} : {nombre2} = " + Division(nombre1, nombre2));
+                    break;
+                default:
+                    Console.WriteLine("Veuillez choisir une opération");
+                    break;
+            }
         }
+        else
+            AfficherMenu();
+
+
     }
 
     public static int Addition(int nombre1, int nombre2)
