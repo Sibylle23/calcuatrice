c4a74eafec82d8704c86c9b38694808997b7f4ef
sibylle <sibyllevanderus@gmail.com>
2023-01-23 11:36:11 +0100

    Startup calculatrice

---
+++ Calculatrice/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Veuillez choisir une opération : " +
+            "\n 1. Addition " +
+            "\n 2. Soustraction " +
+            "\n 3. Multiplication " +
+            "\n 4. Division ");
+        int choix = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Veuillez entrer un premier nombre");
+        int nombre1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Veuillez entrer un deuxème nombre");
+        int nombre2 = int.Parse(Console.ReadLine());
+
+        
+
+       
+
+        switch (choix)
+        {
+            case 1:
+                Console.WriteLine($"Résultat de l'addition {nombre1} + {nombre2} = " + Addition(nombre1, nombre2));
+                break;
+            case 2:
+                Console.WriteLine($"Résultat de la soustraction {nombre1} - {nombre2} = " + Soustraction(nombre1, nombre2));
+                break;
+            case 3:
+                Console.WriteLine($"Résultat de la mutltiplication {nombre1} x {nombre2} = " + Multiplication(nombre1, nombre2));
+                break;
+            case 4:
+                Console.WriteLine($"Résultat de la division {nombre1} : {nombre2} = " + Division(nombre1, nombre2));
+                break; 
+            default:
+                Console.WriteLine("Veuillez choisir une opération");
+                break;
+        }
+
+    }
+
+
+    public static int Addition(int nombre1, int nombre2)
+    {
+        return nombre1 + nombre2;
+    }
+
+    public static int Soustraction(int nombre1, int nombre2)
+    {
+        return nombre1 - nombre2;
+    }
+
+    public static int Multiplication(int nombre1, int nombre2)
+    {
+        return nombre1 * nombre2;
+    }
+
+    public static int Division(int nombre1, int nombre2)
+    {
+        return nombre1 / nombre2;
+    }
+}
